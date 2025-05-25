c43e0f603723c3aeca5018a3ad8c56d0a94b759e
CJ Wunsch <cjwunsch233@gmail.com>
2025-05-24 17:28:20 -0700

    Initial commit

---
+++ catFacts/cat_facts.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import requests
+
+st.set_page_config(page_title="Cat Facts", page_icon="🐱")
+
+st.title("🐾 Random Cat Fact Generator")
+
+if st.button("Get a Cat Fact"):
+    try:
+        response = requests.get("https://catfact.ninja/fact")
+        response.raise_for_status()
+
+        fact = response.json()["fact"]
+        st.success(fact)
+    except Exception as e:
+        st.error(f"Something went wrong: {e}")
+else:
+    st.init("Click the button to get a cat fact!")
